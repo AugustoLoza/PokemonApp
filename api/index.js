3a6dc39b2afffd5cbd5db8e2615bd092dcc040f1
Augusto <augustoloza.11@gmail.com>
2022-06-16 21:09:21 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-
+const PORT  = process.env.PORT
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
